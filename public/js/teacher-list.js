241959b1574a393146f0aece32525234eca879fe
WangDaBai <28475125@qq.com>
2017-09-20 16:11:49 +0800

    完成讲师列表展示功能

---
+++ public/js/teacher-list.js
@@ -0,0 +1,16 @@
+/**
+ * Created by hy on 2017/9/20.
+ */
+define(['jquery','template'],function ($, template) {
+    $.ajax({
+        type:'get',
+        url:'/api/teacher',
+        dataType:'json',
+        success: function (data) {
+            //console.log(data);
+            var html = template('templateTpl',data);
+            //console.log(html);
+            $('#teacherInfo').html(html);
+        }
+    })
+})
