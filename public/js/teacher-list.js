a4a7c25616a18fc7d40021e731880068a9fdc414
WangDaBai <28475125@qq.com>
2017-09-21 20:23:34 +0800

    完成讲师启用和注销功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -11,6 +11,32 @@ define(['jquery','template'],function ($, template) {
             var html = template('templateTpl',data);
             //console.log(html);
             $('#teacherInfo').html(html);
+
+            //启用和注销功能
+            $('.eod').click( function () {
+                var that = this;
+                var td = $(this).closest('td');     //closest 获取最近的父级元素
+                var tcId = td.attr('data-tcId');
+                var status = td.attr('data-status');
+                //console.log(td,tcId,status);
+                $.ajax({
+                    type:'post',
+                    url:'/api/teacher/handle',
+                    data:{tc_id:tcId, tc_status:status},
+                    datatype:'json',
+                    success: function (data) {
+                        //console.log(data);
+                        if(data.code ==200) {
+                            td.attr('data-status',data.result.tc_status);
+                            if(data.result.tc_status == 0) {
+                                $(that).text('注销');
+                            }else{
+                                $(that).text('启用');
+                            }
+                        }
+                    }
+                })
+            })
         }
     })
 })
