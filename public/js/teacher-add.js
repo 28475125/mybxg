29a84b9352a7713a145a10e33b2832def3555b6a
WangDaBai <28475125@qq.com>
2017-09-22 10:39:40 +0800

    完成编辑和添加讲师提交功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -12,16 +12,36 @@ define(['jquery','template','util'],function ($, template,util) {
                 data.result.operate = '编辑讲师';
                 var html = template('teacheraddTpl', data.result);
                 $('#teacheraddInfo').html(html);
+                submitForm('/api/teacher/update');
             }
         })
     }else {
         // id 不存在   添加教师
         console.log(1);
         var html = template('teacheraddTpl',{operate:'添加讲师'});
-        console.log(html);
+        //console.log(html);
         $('#teacheraddInfo').html(html);
+        submitForm('/api/teacher/add');
+
     }
 
+    // 实现表单提交功能
+    function submitForm(url) {
+        $("#teacherBtn").click(function () {
+            $.ajax({
+                type:'post',
+                url: url,
+                data: $("#teacherForm").serialize(),
+                dataType: 'json',
+                success: function (data) {
+                    console.log(data);
+                    if(data.code == 200) {
+                        location.href = '/teacher/teacher_list';
+                    }
+                }
+            })
+        })
 
+    }
 
 })
