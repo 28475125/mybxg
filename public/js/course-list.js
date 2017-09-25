1cdc030b6c1cff96ad88ad5b9c54f2a933cecb3e
WangDaBai <28475125@qq.com>
2017-09-25 21:34:23 +0800

    完成课时列表展示功能

--- public/js/course-list.js
+++ public/js/course-list.js
@@ -2,4 +2,14 @@ define(['jquery','template','util'],function ($, template, util) {
     //console.log(1);
     //设置导航菜单选中高亮显示
     util.setMenu('/course/course_list');
+    $.ajax({
+        type:'get',
+        url : '/api/course',
+        dataType: 'json',
+        success: function (data) {
+            //console.log(data);
+            var html = template('courseTpl',{list:data.result});
+            $('#courseInfo').html(html);
+        }
+    })
 })
